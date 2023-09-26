343a9e1d7d3b5f78fda2a324754a79241e76800a
desoyeDami <desoyedami@gmail.com>
2023-09-26 12:27:48 +0100

    Create a snake, move the snake and control the snake

--- day20&21_Snake-Game/snake.py
+++ day20&21_Snake-Game/snake.py
@@ -1,11 +1,16 @@
 from turtle import Turtle
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 
 class Snake:
     def __init__(self):
         self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
 
     def create_snake(self):
         x = 0
@@ -23,4 +28,20 @@ class Snake:
             seg_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(seg_x, seg_y)
 
-        self.segments[0].fd(MOVE_DISTANCE)
+        self.head.fd(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
