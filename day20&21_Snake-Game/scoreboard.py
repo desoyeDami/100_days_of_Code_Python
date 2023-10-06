1f6dd9fbad2b2d02dd5873e48fb365acb743f268
desoyeDami <desoyedami@gmail.com>
2023-10-06 17:26:02 +0100

    Saving High Scores

--- day20&21_Snake-Game/scoreboard.py
+++ day20&21_Snake-Game/scoreboard.py
@@ -9,20 +9,27 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.penup()
         self.hideturtle()
-        self.color("white")
         self.goto(0, 260)
+        self.color("white")
         self.update_scoreboard()
-
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", False, ALIGNMENT, FONT)
 
     def update_scoreboard(self):
         self.clear()
-        self.write(f"Score: {self.score}", False, ALIGNMENT, FONT)
+        self.write(f"Score: {self.score} High Score: {self.high_score}", False, ALIGNMENT, FONT)
 
     def increase_score(self):
         self.score += 1
         self.update_scoreboard()
+
+    def reset(self):
+        if self.high_score < self.score:
+            self.high_score = self.score
+            with open("data.txt", "w") as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
+        
